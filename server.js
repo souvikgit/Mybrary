138ba888065d92eff50b2ff40b11ed1962ed447b
Sourima Seet <souviksmooth@gmail.com>
2020-09-10 09:24:03 +0530

    changing image location from file system to mongo db

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true
+ })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
