cd5387ce68a2f274235e07851a564562746e36a9
Sourima Seet <souviksmooth@gmail.com>
2020-09-10 21:58:19 +0530

    Finished Author code-method override fix

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
